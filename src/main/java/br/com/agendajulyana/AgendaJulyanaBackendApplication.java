cfff91b095f78a8b607398086ab6e9e03428bc7c
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-24 21:33:18 -0300

    chore: inicializa projeto

---
+++ src/main/java/br/com/agendajulyana/AgendaJulyanaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendajulyana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaJulyanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaJulyanaBackendApplication.class, args);
+	}
+
+}
