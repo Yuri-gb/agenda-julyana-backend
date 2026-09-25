7093b2aed16ca840f2967f25e827fa0949e33d90
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-25 03:26:02 -0300

    fix: remove nome da atualizacao autenticada

--- src/main/java/br/com/agendajulyana/auth/dto/AtualizarUsuarioRequest.java
+++ src/main/java/br/com/agendajulyana/auth/dto/AtualizarUsuarioRequest.java
@@ -3,6 +3,5 @@ package br.com.agendajulyana.auth.dto;
 import jakarta.validation.constraints.Size;
 
 public record AtualizarUsuarioRequest(
-    @Size(max = 150) String nome,
     @Size(max = 30) String telefone
 ) {}
