bbf332761a2f8628e6b8e7061723c3a7ea999512
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-25 14:50:48 -0300

    refactor: remove repository de auditoria duplicado

--- src/main/java/br/com/agendajulyana/auth/repository/AuditoriaRepository.java
+++
@@ -1,5 +0,0 @@
-package br.com.agendajulyana.auth.repository;
-import br.com.agendajulyana.auth.domain.Auditoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-public interface AuditoriaRepository extends JpaRepository<Auditoria, UUID> {}
