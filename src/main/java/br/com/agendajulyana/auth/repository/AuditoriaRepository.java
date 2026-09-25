0ae92902b252f640f0842a7bf036642ca46253b5
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-25 14:35:55 -0300

    fix: restaura repositorio de auditoria

---
+++ src/main/java/br/com/agendajulyana/auth/repository/AuditoriaRepository.java
@@ -0,0 +1,5 @@
+package br.com.agendajulyana.auth.repository;
+import br.com.agendajulyana.auth.domain.Auditoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+public interface AuditoriaRepository extends JpaRepository<Auditoria, UUID> {}
