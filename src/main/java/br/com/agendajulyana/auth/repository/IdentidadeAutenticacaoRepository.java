1d41602f00568d4c6c5f0341958278bf25133383
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-24 22:02:37 -0300

    feat: adiciona repositorio de identidades

--- src/main/java/br/com/agendajulyana/auth/repository/IdentidadeAutenticacaoRepository.java
+++ src/main/java/br/com/agendajulyana/auth/repository/IdentidadeAutenticacaoRepository.java
@@ -1,3 +1,12 @@
 package br.com.agendajulyana.auth.repository;
-import br.com.agendajulyana.auth.domain.*; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*;
-public interface IdentidadeAutenticacaoRepository extends JpaRepository<IdentidadeAutenticacao,UUID>{Optional<IdentidadeAutenticacao> findByProvedorAndIdentificadorExterno(ProvedorAutenticacao p,String id);}
+
+import br.com.agendajulyana.auth.domain.IdentidadeAutenticacao;
+import br.com.agendajulyana.auth.domain.ProvedorAutenticacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IdentidadeAutenticacaoRepository extends JpaRepository<IdentidadeAutenticacao, UUID> {
+    Optional<IdentidadeAutenticacao> findByProvedorAndIdentificadorExterno(
+        ProvedorAutenticacao provedor, String identificadorExterno);
+}
