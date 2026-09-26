9aaa0f39f7e949092496c889d511f4cd0b202062
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 05:17:08 -0300

    feat: adiciona validacao de sobreposicao

--- src/main/java/br/com/agendajulyana/disponibilidade/repository/DisponibilidadeRepository.java
+++ src/main/java/br/com/agendajulyana/disponibilidade/repository/DisponibilidadeRepository.java
@@ -2,6 +2,9 @@ package br.com.agendajulyana.disponibilidade.repository;
 
 import br.com.agendajulyana.disponibilidade.domain.Disponibilidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
-public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade, UUID> {}
+public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade, UUID> {
+    List<Disponibilidade> findByDiaSemanaAndAtivoTrue(Short diaSemana);
+}
