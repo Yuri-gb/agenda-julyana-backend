841b471b405b24abeae368bafa9f47cbfa199c6c
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 15:41:31 -0300

    feat: add Gmail API integration

---
+++ src/main/java/br/com/agendajulyana/integration/gmail/GmailApiConfig.java
@@ -0,0 +1,9 @@
+package br.com.agendajulyana.integration.gmail;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(GmailApiProperties.class)
+public class GmailApiConfig {
+}
