0f760d831064b69c113af7b3dd8b8e8fce77de65
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 06:16:40 -0300

    fix: corrige consulta de conflito de agendamento

--- src/main/java/br/com/agendajulyana/agendamento/repository/AgendamentoRepository.java
+++ src/main/java/br/com/agendajulyana/agendamento/repository/AgendamentoRepository.java
@@ -8,6 +8,12 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento,UUID>{
- @Query("""select count(a)>0 from Agendamento a where a.inicio < :fim and a.fim > :inicio and a.status in :status""")
+ @Query("""
+    select count(a) > 0
+    from Agendamento a
+    where a.inicio < :fim
+      and a.fim > :inicio
+      and a.status in :status
+    """)
  boolean existeConflito(@Param("inicio") OffsetDateTime inicio,@Param("fim") OffsetDateTime fim,@Param("status") java.util.Collection<AgendamentoStatus> status);
 }
