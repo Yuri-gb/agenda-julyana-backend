b33528852314f126498cb10dc2312c801bfa6286
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 05:17:15 -0300

    feat: adiciona validacao de sobreposicao

--- src/main/java/br/com/agendajulyana/disponibilidade/repository/BloqueioRepository.java
+++ src/main/java/br/com/agendajulyana/disponibilidade/repository/BloqueioRepository.java
@@ -2,6 +2,16 @@ package br.com.agendajulyana.disponibilidade.repository;
 
 import br.com.agendajulyana.disponibilidade.domain.Bloqueio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
-public interface BloqueioRepository extends JpaRepository<Bloqueio, UUID> {}
+public interface BloqueioRepository extends JpaRepository<Bloqueio, UUID> {
+    @Query("""
+        select count(b) > 0 from Bloqueio b
+        where b.inicio < :fim and b.fim > :inicio
+        and (:id is null or b.id <> :id)
+    """)
+    boolean existeSobreposicao(@Param("inicio") OffsetDateTime inicio, @Param("fim") OffsetDateTime fim, @Param("id") UUID id);
+}
