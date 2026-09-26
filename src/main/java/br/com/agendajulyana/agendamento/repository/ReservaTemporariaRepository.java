aca8e6634c46c5c96c737588d0ecd58ff3ca588a
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 05:41:05 -0300

    feat: prepara expiração de reservas

--- src/main/java/br/com/agendajulyana/agendamento/repository/ReservaTemporariaRepository.java
+++ src/main/java/br/com/agendajulyana/agendamento/repository/ReservaTemporariaRepository.java
@@ -1,7 +1,6 @@
 package br.com.agendajulyana.agendamento.repository;
-
-import br.com.agendajulyana.agendamento.domain.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ReservaTemporariaRepository extends JpaRepository<ReservaTemporaria,UUID>{}
+import br.com.agendajulyana.agendamento.domain.*; import org.springframework.data.jpa.repository.JpaRepository; import java.time.OffsetDateTime; import java.util.Optional; import java.util.UUID;
+public interface ReservaTemporariaRepository extends JpaRepository<ReservaTemporaria,UUID>{
+ Optional<ReservaTemporaria> findByAgendamentoId(UUID agendamentoId);
+ java.util.List<ReservaTemporaria> findByStatusAndExpiraEmBefore(ReservaStatus status,OffsetDateTime agora);
+}
