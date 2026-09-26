29c5984fac7349379ee664a8b897508def79e17d
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 05:38:41 -0300

    feat: implementa agendamento e reserva temporaria

---
+++ src/main/java/br/com/agendajulyana/agendamento/dto/CriarAgendamentoRequest.java
@@ -0,0 +1,7 @@
+package br.com.agendajulyana.agendamento.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record CriarAgendamentoRequest(@NotNull UUID servicoId,@NotNull OffsetDateTime inicio){}
