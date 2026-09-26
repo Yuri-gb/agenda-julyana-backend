f80330fa06b5f58a5e5fe3301c9476cb74e37326
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 07:12:52 -0300

    feat: exige modalidade de pagamento no agendamento

--- src/main/java/br/com/agendajulyana/agendamento/dto/CriarAgendamentoRequest.java
+++ src/main/java/br/com/agendajulyana/agendamento/dto/CriarAgendamentoRequest.java
@@ -1,7 +1,13 @@
 package br.com.agendajulyana.agendamento.dto;
 
+import br.com.agendajulyana.pagamento.domain.PagamentoModalidade;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-public record CriarAgendamentoRequest(@NotNull UUID servicoId,@NotNull OffsetDateTime inicio){}
+public record CriarAgendamentoRequest(
+        @NotNull UUID servicoId,
+        @NotNull OffsetDateTime inicio,
+        @NotNull PagamentoModalidade modalidadePagamento
+) {}
