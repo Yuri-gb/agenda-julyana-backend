c54577174adf9371eacb39b3dfc7f80e35e7ebe8
Yuri Gabriel <yuriestudos08@gmail.com>
2026-09-26 07:12:45 -0300

    feat: adiciona persistência de pagamento

---
+++ src/main/java/br/com/agendajulyana/pagamento/repository/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package br.com.agendajulyana.pagamento.repository;
+
+import br.com.agendajulyana.pagamento.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
+
+    Optional<Pagamento> findByAgendamentoId(UUID agendamentoId);
+}
